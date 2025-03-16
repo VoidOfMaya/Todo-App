737cdd68f368bec9c3907c622b8415ff5c75cbb8
VoidOfMaya <mayaofthevoid@gmail.com>
2025-03-16 11:52:40 +0200

    creates basic todo item store and view functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import { todoItem } from "./modules/Logic";
+import { todoItem,project } from "./modules/Logic";
 import { renderDisplay } from "./modules/DOM";
 import './style.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
+const defaultProject = new project('default', 'this is the default set project');
+defaultProject.addTodo('walk the dog', 'take the dog out for a walk to the park with jhon', '11-11-2025', 'low', 'none')
 
 console.log(` we are in ${process.env.NODE_ENV}`);
+console.log(defaultProject.viewTodoList());
--- src/modules/Logic.js
+++ src/modules/Logic.js
@@ -1,23 +1,29 @@
 import{format} from 'date-fns';
 class todoItem{
-    constructor(title, description, date, priority, notes, checkList, status){
+    constructor(title, description, date, priority, notes, status){
         this.title = title;
         this.description = description;
         this.date = format(new Date(date), "dd-MM-yyyy");
         this.priority = priority;
         this.notes = notes;
-        this.checkList = checkList;
         this.status = status;
     }
 };
 class project{
+    #todoList = [];
     constructor(name, description){
         this.name = name;
         this.description = description;
     }
-    createTodo(){
+    addTodo(title, description, date, priority, notes,){
+        const todo = new todoItem(title, description, date, priority , notes);
+        todo.status = 'incomplete';
+        this.#todoList.push(todo);
 
+    }
+    viewTodoList(){
+        console.log(this.#todoList);
     }
 }
 
-export {todoItem,}
+export {todoItem, project}
